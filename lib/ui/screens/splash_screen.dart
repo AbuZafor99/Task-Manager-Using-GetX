21bd04d7515caf9bf7041e5a653ac6977a4514f2
zaforsadiq987@gmail.com <138440583+AbuZafor99@users.noreply.github.com>
2025-08-02 15:26:51 +0600

    add lottie logo in splash screen

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,18 +1,15 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+import 'package:lottie/lottie.dart';
 import 'package:task_manager/ui/controllers/auth_controller.dart';
 import 'package:task_manager/ui/screens/main_nav_bar_holder_screen.dart';
 import 'package:task_manager/ui/screens/sign_in_screen.dart';
-import 'package:task_manager/ui/utils/assets_paths.dart';
 import 'package:task_manager/ui/widgets/screen_background.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
-  static const String name='/';
+  static const String name = '/';
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -22,24 +19,30 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    _moveToNextScreen();
+    _navigateToNextScreen();
   }
 
-  Future<void> _moveToNextScreen() async {
-    await Future.delayed(Duration(seconds: 2));
-    bool isLoggedIn= await AuthController.isUserLoggedIn();
-    if(isLoggedIn){
-      Get.offAllNamed(MainNavBarHolderScreen.name);
-    }else {
-      Get.offAllNamed(SignInScreen.name);
-    }
+  void _navigateToNextScreen() {
+    Future.delayed(const Duration(seconds: 3)).then((_) async {
+      final bool isLoggedIn = await AuthController.isUserLoggedIn();
+      if (mounted) {
+        Get.offAll(() => isLoggedIn ? const MainNavBarHolderScreen() : const SignInScreen());
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(
-        child: Center(child: SvgPicture.asset(AssetPaths.logoSVG)),
+        child: Center(
+          child: Lottie.asset(
+            "assets/animations/logo.json",
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+        ),
       ),
     );
   }
